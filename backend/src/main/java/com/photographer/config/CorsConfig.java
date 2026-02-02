5e9c7d81586de2211d9fb5e96fbf8bc9c8b1d561
Adrien Degand <adrien.degand@hotmail.com>
2026-02-02 15:12:00 +0100

    Fix cors 2

--- backend/src/main/java/com/photographer/config/CorsConfig.java
+++ backend/src/main/java/com/photographer/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://photo-project-web.netlify.app")
+                .allowedOrigins("https://photo-project-web.netlify.app,http://localhost:5173/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
