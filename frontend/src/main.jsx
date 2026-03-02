8133d637af2a19bb00556afb776e0955bc302fbf
Adrien Degand <adrien.degand@hotmail.com>
2026-03-02 13:17:31 +0100

    fix : routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router";
+import ContactPage from "./components/ContactPage.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="*" element={<App />} />
+              <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
 )
